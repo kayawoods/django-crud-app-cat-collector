70b9213fab091f2eccb1859da0fd38689d6420e1
Kaya Woods <kayanevada@gmail.com>
2025-04-01 10:23:31 -0700

    completed up to ORM

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
      path('', views.home, name='home'),
      path('about/', views.about, name ='about'),
      path('cats/', views.cat_index, name='cat-index'),
+     path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'), 
      
 ]
- 
+
