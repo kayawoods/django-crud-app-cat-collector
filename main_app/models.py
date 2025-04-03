bb4e390f893a9f750f032c6d12180a64b36ac487
Kaya Woods <kayanevada@gmail.com>
2025-04-03 13:21:00 -0700

    full crud for many to many

--- main_app/models.py
+++ main_app/models.py
@@ -7,11 +7,22 @@ MEALS = (
     ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toy-detail', kwargs={'pk': self.id})
+    
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return f'{self.name} ({self.id})'
@@ -37,13 +48,5 @@ class Feeding(models.Model):
         ordering = ['-date']
 
 
-class Toy(models.Model):
-    name = models.CharField(max_length=50)
-    color = models.CharField(max_length=20)
 
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('toy-detail', kwargs={'pk': self.id})
 
