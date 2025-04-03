542468510413f94790aa8f2605fa669ef1d1b354
Kaya Woods <kayanevada@gmail.com>
2025-04-03 10:01:28 -0700

    toy functionality

--- main_app/models.py
+++ main_app/models.py
@@ -29,8 +29,21 @@ class Feeding(models.Model):
     )
     cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
 
+
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
     
     class Meta:
         ordering = ['-date']
+
+
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toy-detail', kwargs={'pk': self.id})
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Cat
+from .models import Cat, Toy
 from .forms import FeedingForm
+from django.views.generic import ListView, DetailView 
 
 
 
@@ -44,6 +45,25 @@ def add_feeding(request, cat_id):
         new_feeding.cat_id = cat_id
         new_feeding.save()
     return redirect('cat-detail', cat_id=cat_id)
+
+class ToyCreate(CreateView):
+    model = Toy 
+    fields = '__all__'
+
+class ToyList(ListView): 
+    model = Toy 
+
+class ToyDetail(DetailView): 
+    model = Toy 
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
 
 
 
